2df0cd1cf2593b65a63f9ef77360536de14b7132
Atharv Khawale <atharvkhawaleofficial@gmail.com>
2026-02-27 14:42:53 +0530

    Implemented smart appli logic eligibility and skill match scoring

---
+++ backend/routes/applicationRoutes.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const Application = require("../models/Application");
+const Drive = require("../models/Drive");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/apply", async (req, res) => {
+  const { studentId, driveId } = req.body;
+
+  const student = await User.findById(studentId);
+  const drive = await Drive.findById(driveId);
+
+  // 1 Eligibility Check
+  if (student.cgpa < drive.minCGPA) {
+    return res.status(400).json({ message: "Not eligible due to CGPA" });
+  }
+
+  // 2 Skill Match Calculation
+  const matchedSkills = drive.requiredSkills.filter(skill =>
+    student.skills.includes(skill)
+  );
+
+  const matchScore = (matchedSkills.length / drive.requiredSkills.length) * 100;
+
+  // 3 Save Application
+  const application = new Application({
+    student: studentId,
+    drive: driveId,
+    matchScore
+  });
+
+  await application.save();
+
+  res.json({
+    message: "Application submitted",
+    matchScore
+  });
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,14 @@ require("dotenv").config();
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const driveRoutes = require("./routes/driveRoutes");
+const applicationRoutes = require("./routes/applicationRoutes");
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/drives", driveRoutes);
+app.use("/api/applications", applicationRoutes);
 
 // Test route
 app.get("/", (req, res) => {
